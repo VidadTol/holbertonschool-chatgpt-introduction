7ad6c9f36afd9b49844afd3ceedf107d66a28010
vidad <vidad31@gmail.com>
2025-01-13 13:52:36 +0100

    creation mine file

---
+++ debugging/mines.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import random
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class Minesweeper:
+    def __init__(self, width=10, height=10, mines=10):
+        self.width = width
+        self.height = height
+        self.mines = set(random.sample(range(width * height), mines))
+        self.field = [[' ' for _ in range(width)] for _ in range(height)]
+        self.revealed = [[False for _ in range(width)] for _ in range(height)]
+
+    def print_board(self, reveal=False):
+        clear_screen()
+        print('   ' + ' '.join(str(i) for i in range(self.width)))
+        for y in range(self.height):
+            print(f"{y:2}", end=' ')
+            for x in range(self.width):
+                if reveal or self.revealed[y][x]:
+                    if (y * self.width + x) in self.mines:
+                        print('*', end=' ')
+                    else:
+                        count = self.count_mines_nearby(x, y)
+                        print(count if count > 0 else ' ', end=' ')
+                else:
+                    print('.', end=' ')
+            print()
+
+    def count_mines_nearby(self, x, y):
+        count = 0
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.width and 0 <= ny < self.height:
+                    if (ny * self.width + nx) in self.mines:
+                        count += 1
+        return count
+
+    def reveal(self, x, y):
+        # Check if the coordinates are within bounds and not revealed yet
+        if not (0 <= x < self.width and 0 <= y < self.height):
+            return True  # Simply return True for invalid coordinates
+        if self.revealed[y][x]:
+            return True  # No need to reveal a cell that's already revealed
+        
+        if (y * self.width + x) in self.mines:
+            return False
+        
+        self.revealed[y][x] = True
+        if self.count_mines_nearby(x
+
